2d170595a02bcf85dada86c48d0498e1b043977a
Anastasiia Korosteleva <korosteleva-a@mail.ru>
2025-06-24 11:23:10 +0100

    example-2.R

---
+++ tutorial-2/example-2.R
@@ -0,0 +1,10 @@
+#https://www.ebi.ac.uk/ena/browser/view/PRJNA603106?show=reads
+#10 patient cutaneous squamous cell carcinoma tumors and site-matched normal skin WES
+
+
+# Ji AL, Rubin AJ, Thrane K, Jiang S, Reynolds DL, Meyers RM, Guo MG, George BM, Mollbrink A, 
+# Bergenstråhle J, Larsson L, Bai Y, Zhu B, Bhaduri A, Meyers JM, Rovira-Clavé X, 
+# Hollmig ST, Aasi SZ, Nolan GP, Lundeberg J, Khavari PA. 
+# Multimodal Analysis of Composition and Spatial Architecture in Human Squamous Cell Carcinoma. 
+# Cell. 2020 
+# PMID: 32579974; PMCID: PMC7391009.
